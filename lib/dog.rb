3f850e3484116470527733404b081a2c496f7d06
Daniel Sun <danielsun12345@gmail.com>
2020-02-08 01:13:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ attr_accessor :name
 
 def initialize(name)
   @name = name
-  @@all << self
+  save
 end 
 
 def self.all
@@ -19,7 +19,7 @@ def self.print_all
 end
 
 def save
-  
+    @@all << self
 end
 
 end 
